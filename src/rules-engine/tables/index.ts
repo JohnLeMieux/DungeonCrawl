47a94f028dd77b126d81e3157271e34879017b66
John Le Mieux <johnrl@comcast.net>
2026-05-24 21:47:43 -0700

    add weapons table

--- src/rules-engine/tables/index.ts
+++ src/rules-engine/tables/index.ts
@@ -1,5 +1,6 @@
 export { charismaTable } from './attributes/charisma';
 export * from './combat/combat-tables';
+export { weaponTable } from './combat/weapon-table';
 export { constitutionTable } from './attributes/constitution';
 export { dexterityTable } from './attributes/dexterity';
 export { intelligenceTable } from './attributes/intelligence';
